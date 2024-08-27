00d240615fface3d278ff97455974986112047d8
Drago <dragodominguezfb@gmail.com>
2024-08-26 23:54:37 -0600

    Build <Create-Edit> Add page for creating and editing tests in the administration panel
     ### Summary of Changes
    - Introduced a new page component at `src/app/administrar/crear-editar/page.js` for creating and editing tests.
    - The page includes a form with fields for `Nombre`, `Descripción`, `Instrucciones`, and `Tipo de prueba`.
    - Integrated `react-hook-form` with MUI components for form validation and state management.
    - Added radio buttons to select the test type, with validation to ensure a type is selected.
    
     ### Impact
    This change allows users to create and edit tests within the administration panel, improving the app's functionality for managing test content.
    
     Changes to be committed:
    	new file:   src/app/administrar/crear-editar/page.js

---
+++ src/app/administrar/crear-editar/page.js
@@ -0,0 +1,115 @@
+"use client";
+import React from "react";
+import MenuAppBar from "@/components/general/MenuAppBar";
+import {
+  Grid,
+  Container,
+  Typography,
+  TextField,
+  FormControl,
+  RadioGroup,
+  FormLabel,
+  FormControlLabel,
+  Radio,
+  FormHelperText,
+} from "@mui/material";
+
+import { useForm, Controller } from "react-hook-form";
+
+export default function Page() {
+  const methods = useForm({ mode: "all" });
+  //Deconstruccion de methods
+  const {
+    control,
+    register,
+    watch,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+  } = methods;
+
+  return (
+    <>
+      <MenuAppBar />
+      <Container maxWidth="md">
+        <Typography variant="h2" className="mt-16 mb-3 font-semibold">
+          Crear nuevo test
+        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <TextField
+              error={!!errors?.titulo}
+              helperText={errors?.titulo?.message}
+              {...register("titulo", {
+                required: "Campo requerido",
+              })}
+              label="Nombre"
+              fullWidth
+              required
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              error={!!errors?.descripcion}
+              helperText={errors?.descripcion?.message}
+              {...register("descripcion", {
+                required: "Campo requerido",
+              })}
+              label="Descripcion del test"
+              fullWidth
+              required
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              error={!!errors?.instrucciones}
+              helperText={errors?.instrucciones?.message}
+              {...register("instrucciones", {
+                required: "Campo requerido",
+              })}
+              label="Instrucciones del test"
+              fullWidth
+              multiline
+              required
+              variant="standard"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Controller
+              name="tipo"
+              defaultValue={1}
+              control={control}
+              rules={{ required: "Campo requerido." }}
+              render={({ field }) => (
+                <FormControl component="fieldset" error={!!errors?.tipo}>
+                  <FormLabel component="legend">Tipo de prueba</FormLabel>
+                  <RadioGroup {...field} row>
+                    <FormControlLabel
+                      value={1}
+                      control={<Radio />}
+                      defaultChecked
+                      label="Interpretacion de Rangos (TEST 1)"
+                    />
+                    <FormControlLabel
+                      value={2}
+                      control={<Radio />}
+                      label="Areas academicas (Test 2)"
+                    />
+                    <FormControlLabel
+                      value={3}
+                      control={<Radio />}
+                      label="Porcentaje por carreras (Test 3)"
+                    />
+                  </RadioGroup>
+                  <FormHelperText>{errors?.tipo?.message}</FormHelperText>
+                </FormControl>
+              )}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+}
