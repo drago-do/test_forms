dbe48a0e9b00413123640ac79e4f1662cec113e2
Drago <dragodominguezfb@gmail.com>
2024-08-26 23:31:56 -0600

    Build <Administrator> Add test list and search functionality to administration page
     ### Summary of Changes
    - **Administration Page**:
      - Enhanced the administration page with a list of tests, including sample data for display.
      - Implemented a `SearchTextBox` component to allow users to search for specific tests.
      - Added `ItemTest` component to display individual test details with an associated menu icon for further actions.
    
    - **UI Components**:
      - Integrated `MaterialIcon` for consistent icon usage within the test items and search input.
      - Utilized Material-UI components like `Paper`, `TextField`, `List`, `ListItemIcon`, `ListItemText`, `IconButton`, and `InputAdornment` to structure and style the test list.
    
     ### Impact
    These changes improve the functionality of the test administration page, allowing users to view, search, and potentially manage individual tests more effectively.
    
     Changes to be committed:
    	modified:   src/app/administrar/page.js

--- src/app/administrar/page.js
+++ src/app/administrar/page.js
@@ -5,8 +5,54 @@ import MenuAppBar from "@/components/general/MenuAppBar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import MaterialIcon from "@/components/general/MaterialIcon";
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  TextField,
+  InputAdornment,
+} from "@mui/material";
+
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function page() {
+  const tests = [
+    {
+      name: "test",
+      description: "test",
+      date: "2021-01-01",
+      status: "active",
+    },
+    {
+      name: "test2",
+      description: "test2",
+      date: "2021-01-01",
+      status: "active",
+    },
+    {
+      name: "test3",
+      description: "test3",
+      date: "2021-01-01",
+      status: "active",
+    },
+    {
+      name: "test4",
+      description: "test4",
+      date: "2021-01-01",
+      status: "active",
+    },
+    {
+      name: "test5",
+      description: "test5",
+      date: "2021-01-01",
+      status: "active",
+    },
+  ];
+
   return (
     <>
       <MenuAppBar />
@@ -23,7 +69,48 @@ export default function page() {
           <MaterialIcon iconName="add_notes" />
           Crear nuevo test
         </Button>
+        <SeachTextBox />
+        <List>
+          {tests.map((test) => (
+            <ItemTest
+              key={test.name}
+              nombre={test.name}
+              descripcion={test.description}
+            />
+          ))}
+        </List>
       </Container>
     </>
   );
 }
+
+const ItemTest = ({ nombre, descripcion }) => (
+  <Paper elevation={2} className="my-2 p-2 flex items-center justify-between">
+    <div className="flex items-center">
+      <ListItemIcon>
+        <MaterialIcon iconName="question_mark" />
+      </ListItemIcon>
+      <ListItemText primary={nombre} secondary={descripcion} />
+    </div>
+    <IconButton>
+      <MoreVertIcon />
+    </IconButton>
+  </Paper>
+);
+
+const SeachTextBox = (props) => (
+  <TextField
+    {...props}
+    className="my-6"
+    fullWidth
+    variant="outlined"
+    label="Buscar test"
+    InputProps={{
+      startAdornment: (
+        <InputAdornment position="start">
+          <SearchIcon />
+        </InputAdornment>
+      ),
+    }}
+  />
+);
