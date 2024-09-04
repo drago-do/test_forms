9ffd34b3360324ed9ea35b8ae7721af145bdc06f
REYES GUEVARA ALVA <1148144@horusur.corp>
2024-09-03 22:57:43 -0600

    build <api><user/[idMongo]><.env>
    Upgrade mongoose to version 8.6.0 and update MongoDB connection
    
    - Updated package.json and package-lock.json:
      - Upgraded mongoose to version 8.6.0.
        - Updated related dependencies for MongoDB handling.
          - Added license information for various dependencies.
          - Refactored MongoDB connection handling:
            - Replaced the old conexion.ts with a new mongodb.ts file for
    	  managing MongoDB connections, improving connection reuse.
    	  - Updated API routes:
    	    - Created a GET route in /api/route.ts that returns a simple
    	      "Hello World" message.
    	        - Added /api/user/[idMongo]/routeCru.ts to handle user
    		  CRUD operations (get, update, delete).
    		    - Added a POST route in /api/user/route.ts for user
    		      creation.
    		      - Removed the deprecated appi/methoduser.ts file,
    			as the new API routes handle its functionality.
    			- Fixed path import issue in page.js to resolve
    			  the MenuAppBar component's path.
    
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     Committer: REYES GUEVARA ALVA <1148144@horusur.corp>
    
     On branch rga_test
     Your branch is up to date with 'origin/rga_test'.
    
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   src/app/api/route.ts
    	new file:   src/app/api/user/[idMongo]/routeCru.ts
    	new file:   src/app/api/user/route.ts
    	deleted:    src/app/appi/methoduser.ts
    	modified:   src/app/page.js
    	new file:   src/lib/mongodb.ts
    	deleted:    src/models/conexion.ts

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,12 @@
 "use client";
 import Image from "next/image";
 import Container from "@mui/material/Container";
-import MenuAppBar from "@/components/general/MenuAppBar";
+import MenuAppBar from "./../components/general/MenuAppBar";
 
 export default function Home() {
   return (
     <Container maxWidth="lg">
       <MenuAppBar />
-      <Image src="/images/logo.png" alt="logo" width="100" height="100" />
 
       <h1>Hello Next.js</h1>
       <h2>Welcome to Next.js</h2>
