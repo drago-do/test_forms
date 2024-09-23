22a75b770eed7b22de0f19e07f17801994c2c960
Grupo hidalgo <gpohgo@grupohidalgo.com.mx>
2024-09-22 23:39:56 -0600

    build Add form submission handling for creating and updating tests in crear-editar page
    - Introduced `useTest` hook to manage test creation and updating functionality.
    - Added `handleFormSubmit` function to conditionally call `createTest` or `updateTest` based on the presence of an `_id` in the form data.
    - The function handles form submission by resolving or rejecting a promise based on the success or failure of the API call.
    - Integrated `handleFormSubmit` with the form, passing it as `uploadToDataBase` to handle the test creation or update upon form submission.
    - Updated the step component to use `handleFormSubmit` for data submission.
    
     Changes to be committed:
    	modified:   src/app/administrar/crear-editar/page.js

--- src/app/administrar/crear-editar/page.js
+++ src/app/administrar/crear-editar/page.js
@@ -8,6 +8,7 @@ import { DevTool } from "@hookform/devtools";
 import MetadataTest from "./../../../components/admin/MetadataTest";
 import TestCreatorContainer from "./../../../components/admin/TestCreatorContainer";
 import MenuAppBar from "./../../../components/general/MenuAppBar";
+import useTest from "./../../../hook/useTest";
 
 const SectionsForStepForm = [
   { form: MetadataTest, name: "Informacion General" },
@@ -19,6 +20,36 @@ export default function Page() {
   const methods = useForm({ mode: "all" });
   const { control } = methods;
   const stepDebug = 1;
+  const { createTest, updateTest } = useTest();
+
+  const handleFormSubmit = (data) => {
+    return new Promise((resolve, reject) => {
+      setLoading(true);
+      if (data?._id) {
+        updateTest(data?._id, data)
+          .then((response) => {
+            setLoading(false);
+            resolve(response);
+          })
+          .catch((error) => {
+            console.error("Error updating test:", error);
+            setLoading(false);
+            reject(error);
+          });
+      } else {
+        createTest(data)
+          .then((response) => {
+            setLoading(false);
+            resolve(response);
+          })
+          .catch((error) => {
+            console.error("Error creating test:", error);
+            setLoading(false);
+            reject(error);
+          });
+      }
+    });
+  };
 
   return loading ? (
     <FullPageLoader open={loading} />
@@ -32,6 +63,7 @@ export default function Page() {
           edit={false}
           debug={true}
           step={0}
+          uploadToDataBase={handleFormSubmit}
         />
         {parseInt(stepDebug) >= 0 && <DevTool control={control} />}
       </FormProvider>
