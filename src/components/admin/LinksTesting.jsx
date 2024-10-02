17989bf4204e1dbe5c2cdde5855f4f9142d0e3e4
Grupo hidalgo <gpohgo@grupohidalgo.com.mx>
2024-10-01 20:36:45 -0600

    Refactor `LinksTesting` component to enhance performance and code structure
    - Added `useCallback` for memoizing `handleDeleteLink` and `handleAddLink` functions to improve performance by avoiding unnecessary re-renders.
    - Improved error handling when fetching test data and URL validation using `try/catch` blocks.
    - Refined link formatting logic and optimized URL validation to ensure correct link structure.
    - Replaced unnecessary state variables and streamlined the component logic for handling links and rendering tests.
    - Cleaned up the code by removing debug functions and comments, focusing on a cleaner and more maintainable structure.
    
     Changes to be committed:
    	modified:   src/components/admin/LinksTesting.jsx

--- src/components/admin/LinksTesting.jsx
+++ src/components/admin/LinksTesting.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import {
   Grid,
   TextField,
@@ -8,14 +8,14 @@ import {
   Autocomplete,
   Button,
 } from "@mui/material";
-import { useFormContext, Controller, useFieldArray } from "react-hook-form";
+import { useFormContext, useFieldArray } from "react-hook-form";
 import useTest from "../../hook/useTest";
 import { toast } from "sonner";
 
 const URLLocal = process.env.NEXT_PUBLIC_API;
 
 export default function LinkInput({ sectionIndex }) {
-  const { control, setValue, getValues, register } = useFormContext();
+  const { control } = useFormContext();
   const {
     fields: link,
     append,
@@ -26,25 +26,31 @@ export default function LinkInput({ sectionIndex }) {
   });
 
   const [tests, setTests] = useState([]);
-  const [linksToRender, setlinksToRender] = useState([]);
+  const [linksToRender, setLinksToRender] = useState([]);
   const [newLink, setNewLink] = useState("");
   const { getAllTests } = useTest();
 
   useEffect(() => {
     const fetchTests = async () => {
-      const testList = await getAllTests();
-      setTests(testList.data);
+      try {
+        const testList = await getAllTests();
+        setTests(testList.data);
+      } catch (error) {
+        toast.error("Error fetching tests");
+      }
     };
     fetchTests();
-  }, []);
+  }, [getAllTests]);
 
-  const handleDeleteLink = (indexLink) => {
-    remove(indexLink);
-    // Elimina del estado tipo array por índice
-    setlinksToRender((prevLinks) =>
-      prevLinks.filter((_, index) => index !== indexLink)
-    );
-  };
+  const handleDeleteLink = useCallback(
+    (indexLink) => {
+      remove(indexLink);
+      setLinksToRender((prevLinks) =>
+        prevLinks.filter((_, index) => index !== indexLink)
+      );
+    },
+    [remove]
+  );
 
   const formatLink = (link) => {
     try {
@@ -53,55 +59,44 @@ export default function LinkInput({ sectionIndex }) {
       const path = url.pathname;
       const truncatedPath =
         path.length > 10 ? `${path.slice(0, 3)}...${path.slice(-7)}` : path;
-      console.log(truncatedPath);
       return domain + truncatedPath;
     } catch (error) {
       return link;
     }
   };
 
-  const handleAddLink = (newLinkTest = false) => {
-    try {
-      let formattedLink = newLinkTest ? newLinkTest : newLink.trim();
-      // Agregar "https://" si no está presente al inicio de la URL
-      if (!/^https?:\/\//i.test(formattedLink)) {
-        formattedLink = "https://" + formattedLink;
+  const handleAddLink = useCallback(
+    (newLinkTest = false) => {
+      try {
+        let formattedLink = newLinkTest ? newLinkTest : newLink.trim();
+        if (!/^https?:\/\//i.test(formattedLink)) {
+          formattedLink = "https://" + formattedLink;
+        }
+        append(formattedLink);
+        setLinksToRender((prevLinks) => [...prevLinks, formattedLink]);
+        setNewLink("");
+      } catch (error) {
+        toast.error("El enlace no tiene una estructura válida.");
       }
-      // Validar la URL
-      append(formattedLink);
-      setlinksToRender((prevLinks) => [...prevLinks, formattedLink]);
-      setNewLink(""); // Limpiar el campo de entrada
-    } catch (error) {
-      toast.error("El enlace no tiene una estructura válida.");
-    }
-  };
-
-  const verArray = () => {
-    console.log(linksToRender);
-  };
+    },
+    [append, newLink]
+  );
 
   return (
     <Grid item xs={12} className="mt-4">
       <section className="flex flex-col md:flex-row justify-between">
-        <Button variant="text" color="primary" onClick={verArray}>
-          ver
-        </Button>
         <Autocomplete
           freeSolo
           fullWidth
           options={tests}
           getOptionLabel={(option) => option?.titulo || option}
-          onInputChange={(event, newInputValue) => {
-            setNewLink(newInputValue);
-          }}
+          onInputChange={(event, newInputValue) => setNewLink(newInputValue)}
           onChange={(event, newValue) => {
             if (typeof newValue === "string") {
               setNewLink(newValue);
-            } else {
-              if (newValue?._id) {
-                const testLink = URLLocal + "test/" + newValue?._id;
-                handleAddLink(testLink);
-              }
+            } else if (newValue?._id) {
+              const testLink = `${URLLocal}test/${newValue._id}`;
+              handleAddLink(testLink);
             }
           }}
           renderOption={(props, option) => (
@@ -109,15 +104,15 @@ export default function LinkInput({ sectionIndex }) {
               {option?.titulo || option}
             </li>
           )}
-          renderTags={(tagValue, getTagProps) => {
-            return tagValue.map((option, index) => (
+          renderTags={(tagValue, getTagProps) =>
+            tagValue.map((option, index) => (
               <Chip
                 {...getTagProps({ index })}
                 key={option?._id || option}
                 label={option?.titulo || formatLink(option)}
               />
-            ));
-          }}
+            ))
+          }
           renderInput={(params) => (
             <TextField
               {...params}
@@ -126,22 +121,19 @@ export default function LinkInput({ sectionIndex }) {
             />
           )}
         />
-
         <Button
           variant="contained"
           color="primary"
           fullWidth
-          onClick={handleAddLink}
+          onClick={() => handleAddLink()}
           disabled={!newLink.trim()}
-          className="my-2 ml-3"
+          className="my-2"
         >
           Añadir enlace
         </Button>
       </section>
-
-      {/* Display Chips */}
       <Grid item xs={12} className="mt-4">
-        {linksToRender && linksToRender?.length > 0 ? (
+        {linksToRender.length > 0 ? (
           linksToRender.map((link, index) => {
             const test = tests.find(
               (test) => `${URLLocal}test/${test._id}` === link
@@ -149,7 +141,7 @@ export default function LinkInput({ sectionIndex }) {
             return (
               <Chip
                 key={index}
-                label={test ? test?.titulo : link}
+                label={test ? test.titulo : link}
                 onDelete={() => handleDeleteLink(index)}
                 className="mr-2 mb-2"
               />
