b3c15481702734798e74af10e3571e18d60c83da
Grupo hidalgo <gpohgo@grupohidalgo.com.mx>
2024-10-09 00:09:06 -0600

    Enhance test results summary page and refactor API calls
    - Added functionality to dynamically fetch and display test results in the "TestResults" component.
    - Improved error handling for fetching test and result summaries using `toast` notifications.
    - Refactored the `useResults` hook to correctly handle the `data.success` field for all API responses.
    - Integrated a loader and no-response state for better user experience on the results page.
    - Implemented Excel download functionality linked to test-specific results.
    
     Changes to be committed:
    	modified:   src/app/administrar/ver-resultados/page.js
    	modified:   src/hook/useResults.jsx

--- src/app/administrar/ver-resultados/page.js
+++ src/app/administrar/ver-resultados/page.js
@@ -16,6 +16,9 @@ import {
   Paper,
   Grid,
   Box,
+  Alert,
+  AlertTitle,
+  Container,
 } from "@mui/material";
 import {
   BarChart,
@@ -28,91 +31,130 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
-import * as XLSX from "xlsx";
 import PasswordIcon from "@mui/icons-material/Password";
 import { toast } from "sonner";
+import * as XLSX from "xlsx";
 import useResults from "../../../hook/useResults";
 import useTest from "../../../hook/useTest";
+import { useSearchParams } from "next/navigation";
+import FullPageLoader from "./../../../components/general/FullPageLoader";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import MenuAppBar from "../../../components/general/MenuAppBar";
 
 export default function TestResults() {
-  const { getResult } = useResults();
+  const params = useSearchParams();
+  const idTestToGetInfo = params.get("id");
+  const { getSummariOfTestResults } = useResults();
   const { getTestById } = useTest();
-  const [test, setTest] = useState({
-    _id: "testx",
-    titulo: "Simulated Test Title",
-    descripcion: "Simulated Test Description",
-    instrucciones: "Simulated Instructions",
-    sections: [
-      {
-        name: "Section 1",
-        valorMax: 100,
-      },
-      {
-        name: "Section 2",
-        valorMax: 100,
-      },
-    ],
-  });
-  const [results, setResults] = useState([
-    {
-      id_user: "user1",
-      createdAt: new Date(),
-      respuestas: {
-        "Section 1 - Q1": 10,
-        "Section 1 - Q2": 20,
-        "Section 2 - Q1": 30,
-        "Section 2 - Q2": 40,
-      },
-    },
-    {
-      id_user: "user2",
-      createdAt: new Date(),
-      respuestas: {
-        "Section 1 - Q1": 15,
-        "Section 1 - Q2": 25,
-        "Section 2 - Q1": 35,
-        "Section 2 - Q2": 45,
-      },
-    },
-  ]);
+  const [test, setTest] = useState(null);
+  const [results, setResults] = useState([]);
+  const [pageState, setPageState] = useState("loading");
 
   useEffect(() => {
-    first;
+    const fetchTestAndResults = async () => {
+      try {
+        const testData = await getTestById(idTestToGetInfo); // ID de prueba hardcodeado como ejemplo
+        setTest(testData?.documento);
 
-    return () => {
-      second;
+        const resultsData = await getSummariOfTestResults(idTestToGetInfo);
+        if (!resultsData?.data?.length > 0) {
+          setPageState("no_response");
+        } else {
+          console.log(resultsData);
+          setResults(resultsData.data);
+          setPageState("Ok");
+        }
+      } catch (error) {
+        toast.error("Error al cargar los resultados");
+      }
     };
-  }, [third]);
 
-  const respondentCount = results.length;
+    fetchTestAndResults();
+  }, []);
+
+  if (pageState === "loading") {
+    return <FullPageLoader open />;
+  }
+
+  if (pageState === "no_response") {
+    return (
+      <>
+        <MenuAppBar title="Resultados" />
+
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minHeight="50vh"
+        >
+          <Paper
+            elevation={3}
+            sx={{
+              p: 4,
+              maxWidth: 400,
+              width: "100%",
+              textAlign: "center",
+            }}
+          >
+            <ErrorOutlineIcon color="error" sx={{ fontSize: 64, mb: 2 }} />
+            <Alert severity="info" sx={{ mb: 2 }}>
+              <AlertTitle>No hay respuestas disponibles</AlertTitle>
+              Aún no se han registrado respuestas para este test.
+            </Alert>
+            <Typography variant="h6" gutterBottom>
+              {test.nombre || "Test sin nombre"}
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Lo sentimos, pero aún no hay respuestas disponibles para este
+              test. Por favor, intente más tarde o contacte al administrador si
+              cree que esto es un error.
+            </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => window.history.back()}
+            >
+              Regresar
+            </Button>
+          </Paper>
+        </Box>
+      </>
+    );
+  }
+
+  const respondentCount = results?.length;
+  console.log(results);
   const averageScore =
     results.length > 0
       ? results.reduce(
           (sum, result) =>
-            sum + Object.values(result.respuestas).reduce((a, b) => a + b, 0),
+            sum +
+            Object.values(result.respuestas).reduce((a, b) => a + Number(b), 0),
           0
         ) / results.length
       : 0;
 
   const chartData =
-    test?.sections.map((section) => ({
+    test?.sections?.map((section, index) => ({
       name: section.name,
       Promedio:
         results.reduce((sum, result) => {
           const sectionScore = Object.entries(result.respuestas)
-            .filter(([key]) => key.startsWith(section.name))
-            .reduce((sectionSum, [, value]) => sectionSum + value, 0);
+            .filter(([key]) => test.sections[index]._id === key) // Asegurando que coincidan por ID
+            .reduce((sectionSum, [, value]) => sectionSum + Number(value), 0);
           return sum + sectionScore;
         }, 0) / results.length,
       Máximo: section.valorMax,
     })) || [];
 
   const exportToExcel = () => {
-    const worksheet = XLSX.utils.json_to_sheet(results);
-    const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Results");
-    XLSX.writeFile(workbook, "test_results.xlsx");
-    toast.success("Descargando Excel de resultados");
+    const link = document.createElement("a");
+    link.href = `${process.env.NEXT_PUBLIC_API}/api/excel/${test?._id}`; // Enlace que descarga el archivo automáticamente
+    link.target = "_blank"; // Abre en una nueva ventana/pestaña
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link); // Limpia el enlace del DOM
+    toast.success("Iniciando descarga del archivo Excel");
   };
 
   const copyTestCode = () => {
@@ -123,110 +165,115 @@ export default function TestResults() {
         toast.info("Enlace del test copiado al portapapeles");
       })
       .catch((err) => {
-        toast.error("Error al enlace el código del test");
+        toast.error("Error al copiar el código del test");
       });
   };
 
-  if (!test) return <Typography>Loading...</Typography>;
-
   return (
-    <Box sx={{ maxWidth: 1200, margin: "auto", padding: 2 }}>
-      <Card sx={{ marginBottom: 2 }}>
-        <CardHeader title={test.titulo} />
-        <CardContent>
-          <Typography variant="body1">{test.descripcion}</Typography>
-          <Typography variant="body1" sx={{ marginTop: 2 }}>
-            <strong>Instrucciones:</strong> {test.instrucciones}
-          </Typography>
-        </CardContent>
-      </Card>
+    <>
+      <MenuAppBar title="Resultados" />
+      <Box sx={{ maxWidth: 1200, margin: "auto", padding: 2 }}>
+        <Card sx={{ marginBottom: 2 }}>
+          <CardHeader title={test.titulo} />
+          <CardContent>
+            <Typography variant="body1">{test.descripcion}</Typography>
+            <Typography variant="body1" sx={{ marginTop: 2 }}>
+              <strong>Instrucciones:</strong> {test.instrucciones}
+            </Typography>
+          </CardContent>
+        </Card>
 
-      <Grid container spacing={2} sx={{ marginBottom: 2 }}>
-        <Grid item xs={12} md={6}>
-          <Card>
-            <CardHeader title="Resumen" />
-            <CardContent>
-              <Typography variant="body1">
-                Número de respondentes: {respondentCount}
-              </Typography>
-              <Typography variant="body1">
-                Calificación promedio: {averageScore.toFixed(2)}
-              </Typography>
-            </CardContent>
-          </Card>
+        <Grid container spacing={2} sx={{ marginBottom: 2 }}>
+          <Grid item xs={12} md={6}>
+            <Card>
+              <CardHeader title="Resumen" />
+              <CardContent>
+                <Typography variant="body1">
+                  Número de respondentes: {respondentCount}
+                </Typography>
+                <Typography variant="body1">
+                  Calificación promedio: {averageScore.toFixed(2)}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Card>
+              <CardHeader title="Acciones" />
+              <CardContent className="w-full flex justify-between">
+                <Button
+                  variant="contained"
+                  startIcon={<FileDownloadIcon />}
+                  onClick={exportToExcel}
+                >
+                  Exportar a Excel
+                </Button>
+                <Button
+                  variant="contained"
+                  startIcon={<PasswordIcon />}
+                  onClick={copyTestCode}
+                >
+                  Obtener código del test
+                </Button>
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Card>
-            <CardHeader title="Acciones" />
-            <CardContent className="w-full flex justify-between ">
-              <Button
-                variant="contained"
-                startIcon={<FileDownloadIcon />}
-                onClick={exportToExcel}
-              >
-                Exportar a Excel
-              </Button>
-              <Button
-                variant="contained"
-                startIcon={<PasswordIcon />}
-                onClick={copyTestCode}
-              >
-                Obtener código del test
-              </Button>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
 
-      <Card sx={{ marginBottom: 2 }}>
-        <CardHeader title="Resultados por Sección" />
-        <CardContent>
-          <ResponsiveContainer width="100%" height={300}>
-            <BarChart data={chartData}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="Promedio" fill="#8884d8" />
-              <Bar dataKey="Máximo" fill="#82ca9d" />
-            </BarChart>
-          </ResponsiveContainer>
-        </CardContent>
-      </Card>
+        <Card sx={{ marginBottom: 2 }}>
+          <CardHeader title="Resultados por Sección" />
+          <CardContent>
+            <ResponsiveContainer width="100%" height={300}>
+              <BarChart data={chartData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="Promedio" fill="#8884d8" />
+                <Bar dataKey="Máximo" fill="#82ca9d" />
+              </BarChart>
+            </ResponsiveContainer>
+          </CardContent>
+        </Card>
 
-      <Card>
-        <CardHeader title="Resultados Detallados" />
-        <CardContent>
-          <TableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Usuario</TableCell>
-                  <TableCell>Fecha</TableCell>
-                  <TableCell>Puntuación Total</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {results.map((result) => (
-                  <TableRow key={result.id_user}>
-                    <TableCell>{result.id_user}</TableCell>
-                    <TableCell>
-                      {new Date(result.createdAt).toLocaleDateString()}
-                    </TableCell>
-                    <TableCell>
-                      {Object.values(result.respuestas).reduce(
-                        (a, b) => a + b,
-                        0
-                      )}
-                    </TableCell>
+        <Card>
+          <CardHeader title="Resultados Detallados" />
+          <CardContent>
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Usuario</TableCell>
+                    <TableCell>Fecha</TableCell>
+                    <TableCell>Puntuación Total</TableCell>
+                    <TableCell>Promedio por sección</TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </CardContent>
-      </Card>
-    </Box>
+                </TableHead>
+                <TableBody>
+                  {results.map((result) => (
+                    <TableRow key={result.id_user._id}>
+                      <TableCell>{`${result.id_user.firstName} ${result.id_user.lastName}`}</TableCell>
+                      <TableCell>
+                        {new Date(result.createdAt).toLocaleDateString()}
+                      </TableCell>
+                      <TableCell>
+                        {Object.values(result.respuestas).reduce(
+                          (a, b) => a + Number(b),
+                          0
+                        )}
+                      </TableCell>
+                      {result.seccionesResultado.map((seccion, index) => (
+                        <TableCell key={index}>{seccion.porcentaje}%</TableCell>
+                      ))}
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </CardContent>
+        </Card>
+      </Box>
+    </>
   );
 }
--- src/hook/useResults.jsx
+++ src/hook/useResults.jsx
@@ -7,60 +7,60 @@ const useResults = () => {
     const res = await axios.get(
       `${api}/api/results?page=${page}&limit=${limit}`
     );
-    if (res.success) {
+    if (res.data.success) {
       return res?.data;
     } else {
-      toast.error(res.message);
+      toast.error(`Error al obtener los test. ${res.message}`);
       return [];
     }
   };
 
   const createResult = async (data) => {
     const res = await axios.post(`${api}/api/results`, data);
-    if (res.success) {
+    if (res.data.success) {
       return res?.data;
     } else {
-      toast.error(res.message);
+      toast.error(`Error al crear test. ${res.message}`);
       return [];
     }
   };
 
   const getResultById = async (id) => {
     const res = await axios.get(`${api}/api/results/viewresults/${id}`);
-    if (res.success) {
+    if (res.data.success) {
       return res?.data;
     } else {
-      toast.error(res.message);
+      toast.error(`Error al obtener los resultados. ${res.message}`);
       return [];
     }
   };
 
   const getSummariOfTestResults = async (idPrueba) => {
-    const res = await axios.get(`${api}/api/results/summarize/=${idPrueba}`);
-    if (res.success) {
+    const res = await axios.get(`${api}/api/results/summarize/${idPrueba}`);
+    if (res.data.success) {
       return res?.data;
     } else {
-      toast.error(res.message);
+      toast.error(`Error al obtener los resumenes del test. ${res.message}`);
       return [];
     }
   };
 
   const updateResult = async (id, data) => {
     const res = await axios.put(`${api}/api/results/${id}`, data);
-    if (res.success) {
+    if (res.data.success) {
       return res?.data;
     } else {
-      toast.error(res.message);
+      toast.error(`Error al actualizar el test. ${res.message}`);
       return [];
     }
   };
 
   const deleteResult = async (id) => {
     const res = await axios.delete(`${api}/api/results/${id}`);
-    if (res.success) {
+    if (res.data.success) {
       return res?.data;
     } else {
-      toast.error(res.message);
+      toast.error(`Error al eliminar el test. ${res.message}`);
       return [];
     }
   };
